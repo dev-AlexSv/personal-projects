ff36b9b290c311a0300676f779e2425b844508af
dev-AlexSv <janalexandersilvestree@gmail.com>
2025-12-01 21:20:20 +0800

    Added Registration Page
    Finalized the main page
    
    by devAlex

--- global-skills-web-sample/code.js
+++ global-skills-web-sample/code.js
@@ -5,6 +5,24 @@ function changeMode() {
     root.className = newTheme;
 }
 
+function directHomePage() {
+    window.location.href = "main.html";
+}
+
+function transitionBanner() {
+    window.addEventListener("DOMContentLoaded", () => {
+    const banner = document.querySelector(".banner-intro");
+    banner.classList.add("show"); // adds the class after page load
+  });
+}
+
 const btMode = document.querySelector("#mode-change");
+const mainLogo = document.getElementById("main-logo");
 
 btMode.addEventListener("click", changeMode)
+mainLogo.addEventListener("click", directHomePage);
+transitionBanner();
+
+
+
+
